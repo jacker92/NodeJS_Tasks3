b871533318aaf5835d4297bd35cf6d4c055a37f8
Jaakko Lahtinen <lahtinen.jaakko@gmail.com>
2019-11-09 10:05:48 +0200

    Added validations for query-parameters.

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
+const validator = require('./query_validator.js');
 
 const app = express(); 
 const port = 3000;
 
 app.use(morgan('dev'));
 
+app.get('/menus', (req, res, next) => {
+    console.log("In app get menus");
+    console.log("Sending request to validator");
+    next();
+}, (req,res) => {
+    validator.validate(req,res);
+});
 
-app.get('/menus', (req, res) => 
-res.send('Hello World!'))
 
 app.listen(port, () => 
 console.log(`Example app listening on port ${port}!`))
---
+++ query_validator.js
@@ -0,0 +1,19 @@
+const validate = (req,res) => {
+    console.log("In validate!");
+
+    let action = req.query["action"];
+    if(action) {
+        if(action != 'today' && action != 'tomorrow') {
+            res.status(400).send("Invalid query parameters!");
+        } else {
+            res.status(200).send(); 
+        }
+    } else {
+        res.status(200).send();
+    }
+
+}
+
+module.exports = {
+    validate
+}
